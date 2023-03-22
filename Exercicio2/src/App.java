3f9c52d801cb94f8f7d2c356e5b7ec73273d947e
LucasTBandeira <lucastbandeira@gmail.com>
2023-03-22 17:42:21 -0300

    Changed bank account taxes implementation.

--- Exercicio2/src/App.java
+++ Exercicio2/src/App.java
@@ -1,19 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Conta conta1 = new Conta.ContaBuilder(1l, "ContaMínima")
+        Conta conta1 = new Conta.ContaBuilder(1l, "ContaNormal")
         .build();
 
-        Conta conta2 = new Conta.ContaBuilder(2l,"ContaCompleta")
+        Conta conta2 = new Conta.ContaBuilder(2l,"ContaAdvanced")
         .saldoLivreInicial(1000)
         .saldoAplicacaoInicial(5000)
-        .taxaRemuneracao(0.2)
-        .taxaSaldoNegativo(0.2)
         .categoriaInicial(Conta.Categoria.ADVANCED)
+        .build();
+
+        Conta conta3 = new Conta.ContaBuilder(3l,"ContaPremium")
+        .saldoLivreInicial(5000)
+        .saldoAplicacaoInicial(10000)
+        .categoriaInicial(Conta.Categoria.PREMIUM)
         .build();
 
         System.out.println(conta1.toString());
         System.out.println("=======================");
         System.out.println(conta2.toString());
+        System.out.println("=======================");
+        System.out.println(conta3.toString());
     }
 }
